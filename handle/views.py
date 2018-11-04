4e40ceb53c40c9c19dba4cce7cee7e01d5c408ac
Aprilisyourlie <865833921@qq.com>
2018-11-04 20:03:18 +0800

    Update views.py

--- handle/views.py
+++ handle/views.py
@@ -8,5 +8,16 @@ import IndexSearching.indexSearching as indexSearching
 
 def result(request):
     user_text = request.GET['text']
+    result = [{},{},{},{},{},{},{},{},{},{}]
     namelist,cataloglist,descriptionlist = indexSearching.get_indexSearching_result(user_text)
-    return render(request, 'result.html', {'catalog': cataloglist, 'name': namelist, 'description': descriptionlist})
+
+    for i in range(10):
+        result[i] = {'name':namelist[i]}
+    for i in range(10):
+        result[i]['catalog'] = cataloglist[i]
+    for i in range(10):
+        result[i]['description'] = descriptionlist[i]
+    print(result)
+    # resultlist = {'catalog': cataloglist, 'name': namelist, 'description': descriptionlist}
+    # print(resultlist)
+    return render(request, 'result.html',{'result':result})
